5e5b524e9503579df827a730fc0e21e22f6a8945
Hassan Al-Nouri <Hassan Al-Nouri>
2016-02-05 20:17:59 +0200

    Video 6 done. Linked opengl32.lib and glew32d.lib, included glew's library, moved glew32d.dll to the debug folder of SandboxGame project and drew a two dimentional triangle inside a window.

--- SandboxGame/MyGlWindow.cpp
+++ SandboxGame/MyGlWindow.cpp
@@ -1,6 +1,30 @@
+#include <GL\glew.h>
+#include <cassert>
 #include "MyGlWindow.h"
+
 
 void MyGlWindow::initializeGL()
 {
+	GLenum errorCode = glewInit();
+	assert(errorCode == 0);
 
+	glGenBuffers(1, &vertexBufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+
+	float verts[] =
+	{
+		+0.0f, +0.1f,
+		-0.1f, -0.1f,
+		+0.1f, -0.1f,
+	};
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+}
+
+void MyGlWindow::paintGL()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
--- SandboxGame/MyGlWindow.h
+++ SandboxGame/MyGlWindow.h
@@ -4,7 +4,9 @@
 
 class MyGlWindow : public QGLWidget
 {
+	GLuint vertexBufferID;
 protected:
+	void paintGL();
 	void initializeGL();
 };
 
