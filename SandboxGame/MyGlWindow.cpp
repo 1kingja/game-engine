1332cf2d36b54fb265f6dec50c5963d3519bbf2c
Hassan Al-Nouri <Hassan Al-Nouri>
2016-02-10 05:27:32 +0200

    Video 16 done. Used myUpdate to move the triangle by 0.01f in the X-axis and the Y-axis in each frame.

--- SandboxGame/MyGlWindow.cpp
+++ SandboxGame/MyGlWindow.cpp
@@ -4,20 +4,21 @@
 #include <Math\Vector2D.h>
 using Math::Vector2D;
 
-static Vector2D verts[] =
+namespace 
 {
-	Vector2D(+0.0f, +0.1f),
-	Vector2D(-0.1f, -0.1f),
-	Vector2D(+0.1f, -0.1f),
-};
-static const unsigned int NUM_VERTS = sizeof(verts) / sizeof(*verts);
-
+	Vector2D verts[] =
+	{
+		Vector2D(+0.0f, +0.1f),
+		Vector2D(-0.1f, -0.1f),
+		Vector2D(+0.1f, -0.1f),
+	};
+	const unsigned int NUM_VERTS = sizeof(verts) / sizeof(*verts);
+	Vector2D shipPosition(0.0f, 0.0f);
+}
 void MyGlWindow::initializeGL()
 {
 	GLenum errCode = glewInit();
 	assert(errCode == 0);
-
-	
 
 	glGenBuffers(1, &vertexBufferID);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
@@ -27,7 +28,7 @@ void MyGlWindow::initializeGL()
 
 	connect(&myTimer, SIGNAL(timeout()),
 		this,SLOT(myUpdate()));
-	myTimer.start(0);
+	myTimer.start(1);
 }
 
 void MyGlWindow::paintGL()
@@ -36,14 +37,10 @@ void MyGlWindow::paintGL()
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE,0,0);
 
-
-	Vector2D shipPosition(0.5f, 0.5f);
-
 	Vector2D translatedVerts[NUM_VERTS];
 	for (unsigned int i = 0; i < NUM_VERTS;i++)
-	{
 		translatedVerts[i] = verts[i] + shipPosition;
-	}
+	
 	glBufferSubData(GL_ARRAY_BUFFER, 0, 
 		sizeof(translatedVerts),
 		translatedVerts);
@@ -53,4 +50,7 @@ void MyGlWindow::paintGL()
 
 void MyGlWindow::myUpdate()
 {
+	Vector2D veclocity(0.01f, 0.01f);
+	shipPosition = shipPosition + veclocity;
+	repaint();
 }
