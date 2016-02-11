643880cd8c9c067cd84e42133e2d3b0f827c627f
Hassan Al-Nouri <Hassan Al-Nouri>
2016-02-11 06:07:52 +0200

    Video 18 done. Included <Windows.h> inside SandboxGame.cpp inside the SandboxGame Project and defined a LARGE_INTEGER variable and used it inside the QueryPerformanceFrequency function.

--- SandboxGame/SandboxGame.cpp
+++ SandboxGame/SandboxGame.cpp
@@ -1,9 +1,14 @@
 #include <qt/qapplication.h>
 #include <QtGui/qwidget.h>
 #include "MyGlWindow.h"
+#include <Windows.h>
 
 int main(int argc, char* argv[])
 {
+	LARGE_INTEGER clockFrequency;
+	QueryPerformanceFrequency(&clockFrequency);
+
+
 	QApplication application(argc, argv);
 	MyGlWindow myGlWindow;
 	myGlWindow.show();
