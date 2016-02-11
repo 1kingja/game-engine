77c6c167f278dfdabb76dc771cf9ab25cf85636f
Hassan Al-Nouri <Hassan Al-Nouri>
2016-02-11 06:16:46 +0200

    Video 19 done. Calculated delta time using LONG_INTEGER and float variables.

--- SandboxGame/SandboxGame.cpp
+++ SandboxGame/SandboxGame.cpp
@@ -2,12 +2,24 @@
 #include <QtGui/qwidget.h>
 #include "MyGlWindow.h"
 #include <Windows.h>
+#include <Qt\qdebug.h>
 
 int main(int argc, char* argv[])
 {
 	LARGE_INTEGER clockFrequency;
 	QueryPerformanceFrequency(&clockFrequency);
 
+
+	LARGE_INTEGER startTime;
+	LARGE_INTEGER endTime;
+	QueryPerformanceCounter(&startTime);
+	qDebug() << "Hello";
+	QueryPerformanceCounter(&endTime);
+
+	LARGE_INTEGER delta;
+	delta.QuadPart = endTime.QuadPart - startTime.QuadPart;
+
+	float deltaSeconds = ((float)delta.QuadPart) / clockFrequency.QuadPart;
 
 	QApplication application(argc, argv);
 	MyGlWindow myGlWindow;
