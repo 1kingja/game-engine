a964a80437b4989065f0c80c2b21e69c27ec59a2
Hassan Al-Nouri <Hassan Al-Nouri>
2016-02-12 09:01:11 +0200

    Video 21 done. Continued editing ClockTests.cpp

--- EngineTester/ClockTests.cpp
+++ EngineTester/ClockTests.cpp
@@ -19,9 +19,22 @@ TEST(Clock, FrameTimeMeasuring)
 	sleep(500);
 	clock.newFrame();
 	timedTime = clock.timeElapsedLastFrame();
-	EXPECT_TRUE(0.4f < timedTime && timedTime < 0.6f);
+	EXPECT_TRUE(0.4f < timedTime);
+	EXPECT_TRUE(timedTime < 0.6f);
 
-
+	const int NUM_TESTS = 10 + rand() % 100;
+	const float THRESHOLD = 0.1f;
+	for (int i = 0; i < NUM_TESTS; i++)
+	{
+		int thisTestTimeMiliseconds = rand() % 10000;
+		float thisTestTimeSeconds = thisTestTimeMiliseconds / 1000.0f;
+		clock.newFrame();
+		sleep(thisTestTimeMiliseconds);
+		clock.newFrame();
+		float elapsedSeconds = clock.timeElapsedLastFrame();
+		EXPECT_TRUE((thisTestTimeSeconds - THRESHOLD) < elapsedSeconds);
+		EXPECT_TRUE(elapsedSeconds < (thisTestTimeSeconds - THRESHOLD));
+	}
 
 	clock.timeElapsedLastFrame();
 	EXPECT_TRUE(clock.shutdown());
