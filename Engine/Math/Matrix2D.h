24d1d524aa27bc1ab2db20049ad4f01a30534c01
Hassan Al-Nouri <Hassan Al-Nouri>
2016-02-25 20:59:29 +0200

    Video 37 done. Added Matrix2D.inl and implemented a testable class for gtest.

--- Engine/Math/Matrix2D.h
+++ Engine/Math/Matrix2D.h
@@ -10,11 +10,16 @@ namespace Math
 		float r0c1;
 		float r1c0;
 		float r1c1;
-		explicit Matrix2D(
+		inline explicit Matrix2D(
 			float r0c0 = 1, float r0c1 = 0, 
 			float r1c0 = 1, float r1c1 = 1);
-		//Vector2D 
 
 	};
+
+	inline Vector2D operator*(
+		const Matrix2D& left,
+		const Vector2D& right);
+
+#include "Matrix2D.inl"
 }
 #endif
---
+++ Engine/Math/Matrix2D.inl
@@ -0,0 +1,13 @@
+
+Matrix2D::Matrix2D(
+	float r0c0, float r0c1,
+	float r1c0, float r1c1) 
+{
+}
+
+inline Vector2D operator*(
+	const Matrix2D& left,
+	const Vector2D& right)
+{
+	return Vector2D();
+}
