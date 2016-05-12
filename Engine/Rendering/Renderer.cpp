b38c9c7ceda88c8e7a29bbdfa40cfef48a3dd901
Hassan Al-Nouri <develhanter@yahoo.com>
2016-05-12 10:13:20 +0300

    Video 170 done. Created initalize, shutdown and add for Geometries and Renderables.

--- Engine/Rendering/Renderer.cpp
+++ Engine/Rendering/Renderer.cpp
@@ -1,2 +1,38 @@
 #include "Renderer.h"
+#include <cassert>
 
+namespace Rendering
+{
+
+	bool Renderer::initialize()
+	{
+		numGeometries = 0;
+		numRenderables = 0;
+		return true;
+	}
+	bool Renderer::shutdown()
+	{
+		return true;
+	}
+
+	Geometry* Renderer::addGeometry(
+		Math::Vector3D* vertices, uint numVerts,
+		ushort* indices, uint numIndices)
+	{
+		assert(numGeometries != NUM_MAX_GEOMETRIES);
+		Geometry& g = geometries[numGeometries++];
+		g.vertices = vertices;
+		g.numVerts = numVerts;
+		g.indices = indices;
+		g.numIndices = numIndices;
+		return &g;
+	}
+
+	Renderable* Renderer::addRenderable(Geometry* geometry)
+	{
+		assert(numRenderables != NUM_MAX_RENDERABLES);
+		Renderable& r = renderables[numRenderables++];
+		r.what = geometry;
+		return &r;
+	}
+}
--- Engine/Rendering/Renderer.h
+++ Engine/Rendering/Renderer.h
@@ -1,12 +1,27 @@
 #ifndef ENGINE_RENDERER_H
 #define ENGINE_RENDERER_H
+#include <Misc\TypeDefs.h>
+#include <Rendering\Geometry.h>
+#include <Rendering\Renderable.h>
+namespace Math { class Vector3D; }
 
 namespace Rendering
 {
 	class Renderer
 	{
+		static uint NUM_MAX_GEOMETRIES = 10;
+		Geometry geometries[NUM_MAX_GEOMETRIES];
+		uint numGeometries;
+		static uint NUM_MAX_RENDERABLES = 10;
+		Renderable renderables[NUM_MAX_RENDERABLES];
+		uint numRenderables;
 	public:
-
+		bool initialize();
+		bool shutdown();
+		Geometry* addGeometry(
+			Math::Vector3D* vertices, uint numVerts,
+			ushort* indices, uint numIndices);
+		Renderable* addRenderable(Geometry* geometry);
 	};
 }
 #endif
